846944d201308734030951b1c43d5824329296f6
Jannis Baum <jannis.baum@gmail.com>
2023-07-13 15:48:20 +0200

    feat: pass options

---
+++ options.zsh
@@ -0,0 +1,13 @@
+function __opt_or_fallback() {
+    eval "! (( \${+$1} )) && $1=\"$2\""
+}
+
+__opt_or_fallback FZFDF_ACT_1 ctrl-o
+__opt_or_fallback FZFDF_ACT_2 ctrl-u
+__opt_or_fallback FZFDF_ACT_3 ctrl-b
+__opt_or_fallback FZFDF_ACT_NEW ctrl-n
+__opt_or_fallback FZFDF_ACT_RELOAD ctrl-r
+
+__opt_or_fallback FZFDF_LS "ls -la"
+
+unset -f __opt_or_fallback
